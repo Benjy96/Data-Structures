93b58810dc6b6cdfa492e4f0b751a8475a535397
Benjamin Dickey <>
2022-02-18 15:57:03 +0000

    Add instruction to run algorithm

--- topics/binary-search.js
+++ topics/binary-search.js
@@ -1,4 +1,7 @@
 /**
+ 
+To run this test, type "node binary-search.js" into your console in this directory.
+
 Problem: Given a sorted array of distinct integers and a target value, 
 return the index if the target is found. 
 If not, return the index where it would be if it were inserted in order.
