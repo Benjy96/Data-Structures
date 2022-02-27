eb7f7255f111fd3a37677beb698c85f68c851eb5
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2022-02-27 13:41:11 +0000

    Add documentation comments explaining bit functions

--- concepts/bit-crud.js
+++ concepts/bit-crud.js
@@ -15,12 +15,25 @@ function getBit(num, i) {
     else return 0;
 }
 
+/**
+ * TL;DR: Sets the ith bit (from the right) to 1 in num's bits.
+ * 
+ * How? Sets a bit at index i in a "bit array" of all 0s to 1, then ORs that against the number "num".
+ * All 1s/0s in original num kept, EXCEPT the new 1 at the specified index, i, as 1|1 == 1, and 1|0 == 1.
+ */
 function setBit(num, i) {
     var numWithOnlyBitToSet = 1 << i;   // Could also call a "Mask" to be placed over other number
     // in bitToSet, only 1 bit is 1. ORing both nums will result in old num with extra 1 if not already.
     return num | numWithOnlyBitToSet;
 }
 
+/**
+ * TL;DR: Sets the bit at index i in num to 0
+ * 
+ * How? Makes a bit array of all 1s except for index. Compares that bit array vs num's bit array, and ANDs
+ * the two arrays. All num's 1s kept, because 1&1 == 1, all num's 0s kept, because 1&0 == 0. And the new bit
+ * at index i is 0, the & will result in a 0 (0&0 == 0)
+ */
 function clearBit(num, i) {
     // Would need to not affect other bits whether they are 0 or 1
     var allOnesExceptIndex = ~(1 << i);   // ~ inverses the bits (bitwise NOT), e.g., 0010 -> 1101
@@ -31,13 +44,23 @@ function clearBit(num, i) {
     return num & allOnesExceptIndex;
 }
 
-//Assume 1, 1, 1
+/**
+ * TL;DR: Sets the ith bit in num to bitVal.
+ * 
+ * How? Creates a bit array of all 1s except for at the index i, which is set to 0.
+ * ANDs num with this array to produce original number except at index i, which will be 0.
+ * We now have original num with a cleared bit.
+ * 
+ * Then create a bit array of all 0s, and then move bitVal to index i, which could be 0 or 1.
+ * OR the number with cleared bit against the new array, resulting in original number with the new bit at the index,
+ * because num is 0 at index, and if index is 0, it will be 0. If index is 1, it will be 1, as 1 | 0 == 1.
+ */
 function updateBit(num, i, bitVal) {
+    // For comments in this function, assume params: 1,1,1
     var allOnesExceptNewBitIndex = ~(1 << i);   // create a mask of 1s except for index to change, e.g.,    - 1101
-    var desiredBitAtPosition = bitVal << i;    // set the desired position to 1 or 0, e.g.,       - 0010
-
     var numWithNewBitPositionCleared = num & allOnesExceptNewBitIndex; // 0001 & 1101 == 0001
 
+    var desiredBitAtPosition = bitVal << i;    // set the desired position to 1 or 0, e.g.,       - 0010
     return numWithNewBitPositionCleared | desiredBitAtPosition; // 0001 | 0010 == 0011
 }
 
