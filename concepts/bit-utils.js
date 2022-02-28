6234358ec3aeda7133264fa0b843502aaf4455f8
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2022-02-28 12:13:27 +0000

    Update bit utils comment explanation

--- concepts/bit-utils.js
+++ concepts/bit-utils.js
@@ -1,5 +1,7 @@
 module.exports = { 
     toBinary: function(decimal) {
+        // Integers are signed, use >>> to convert to unsigned
+        
         // Number.toString(radix) - where radix is int through 2-36 specifying base for numeric value
         // 1, or 001 >>> 0 == 001, as the unsigned right shift shifts in zero bits from the left.
         return (decimal >>> 0).toString(2);
