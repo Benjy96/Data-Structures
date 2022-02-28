382fb1ba09b4aefea2e9754d54b1b2cb41635bd7
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2022-02-28 14:21:31 +0000

    Add simple linked list to data structures

---
+++ data-structures/linked-list.js
@@ -0,0 +1,35 @@
+class Node {
+    next;   // stores next node in the linked list
+    data;   // stores current node's value
+
+    constructor(data) {
+        this.data = data;
+    }
+
+    appendToTail(data) {
+        // create a node with data
+        var end = new Node(data);
+        var node = this;
+
+        // Move to the last node
+        while(node.next != null) {
+            node = node.next;
+        }
+
+        // set last node's next node to the newly created end node
+        node.next = end;
+    }
+
+    getNextNode() {
+        return this.next;
+    }
+}
+
+var n = new Node(1);
+n.appendToTail(2);
+n.appendToTail(3);
+
+while(n != null) {
+    console.log(n.data);
+    n = n.getNextNode();
+}
