8e10d6ec16fa4238576644fafb6e74fce409fafd
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2022-02-28 11:02:30 +0000

    Update toDecimal to use 0b prefix

--- concepts/bit-utils.js
+++ concepts/bit-utils.js
@@ -1,10 +1,11 @@
 module.exports = { 
-    // For display purposes
     toBinary: function(decimal) {
+        // Number.toString(radix) - where radix is int through 2-36 specifying base for numeric value
+        // 1, or 001 >>> 0 == 001, as the unsigned right shift shifts in zero bits from the left.
         return (decimal >>> 0).toString(2);
     },
 
     toDecimal: function(num) {
-        return parseInt(num,2);
+        return Number("0b" + num);
     }
 }
