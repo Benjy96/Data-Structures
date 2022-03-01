0b27649b3c48813608e97001c288a534db6e3d76
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2022-03-01 21:10:05 +0000

    Update steps

--- algorithms/sliding-window.js
+++ algorithms/sliding-window.js
@@ -34,10 +34,11 @@ console.log("The longest substring in " + example + " is " + longestSubstring(ex
 /**
  * STEPS:
  * 1. Move right +1 each loop until end of string:
- *      1.1 While same char recorded >1:
- *          1.1.1 Decrement char at left (l)
- *          1.1.2 Move left +1
- *      1.2 Record longest sub string from last one and (right-left)+1
+ *      1.1 Increment num times char seen at right
+ *      1.2 While same char recorded >1:
+ *          1.2.1 Decrement char at left (l)
+ *          1.2.2 Move left +1
+ *      1.3 Record longest sub string from last one and (right-left)+1
  * 2. Return longest sub string
  */
 
