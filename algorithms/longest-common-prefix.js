a8e0f9404419086f98f0b13e21bb8da344cdee1e
Benjy96 <benjamindickey@hotmail.co.uk>
2022-05-09 21:06:26 +0100

    Make longest common prefix more efficient

--- algorithms/longest-common-prefix.js
+++ algorithms/longest-common-prefix.js
@@ -5,9 +5,11 @@ function LongestCommonPrefix(stringArray) {
     var prefix = stringArray[0];
 
     for(var i = 1; i < stringArray.length; i++) {
+        // indexOf will return -1 if we can't find prefix within current string
         while(stringArray[i].indexOf(prefix) == -1) {
             // shorten prefix so we can try to find it again in next loop
             prefix = prefix.substring(0, prefix.length - 1);
+            if(prefix == "") return "";
         }
     }
 
